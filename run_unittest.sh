142db9d96c603c244b0c5d7b5d42d981aca480b3
rsmuc <rsmuc@mailbox.org>
2016-05-13 11:20:42 +0200

    Added unittests for Meinberg NTP

--- run_unittest.sh
+++ run_unittest.sh
@@ -1,15 +1,33 @@
 #/bin/bash
-echo "Test check_snmp_large_storage"
-py.test health_monitoring_plugins/check_snmp_large_storage/_unittest.py -v
 
+# ok
+echo "Test check_meinberg_ntp"
+py.test health_monitoring_plugins/check_meinberg_ntp/_unittest.py -v
+
+# fehlt
 #echo "Test check_snmp_time2"
 #py.test health_monitoring_plugins/check_snmp_time2/_unittest.py -v
 
-echo "Test check_snmp_service"
-py.test health_monitoring_plugins/check_snmp_service/_unittest.py -v
+# fehlt
+#echo "Test check_moxa_6000"
+#py.test health_monitoring_plugins/check_moxa_6000/_unittest.py -v
 
-echo "Test check_snmp_raritan"
-py.test health_monitoring_plugins/check_snmp_raritan/_unittest.py -v
+# fehlt
+#echo "Test check_snmp_ilo4"
+#py.test health_monitoring_plugins/check_snmp_ilo4/_unittest.py -v
 
-echo "Test check_snmp_port"
-py.test health_monitoring_plugins/check_snmp_port/_unittest.py -v
+# ok
+#echo "Test check_snmp_large_storage"
+#py.test health_monitoring_plugins/check_snmp_large_storage/_unittest.py -v
+
+# ok
+#echo "Test check_snmp_service"
+#py.test health_monitoring_plugins/check_snmp_service/_unittest.py -v
+
+# ok
+#echo "Test check_snmp_raritan"
+#py.test health_monitoring_plugins/check_snmp_raritan/_unittest.py -v
+
+# ok
+#echo "Test check_snmp_port"
+#py.test health_monitoring_plugins/check_snmp_port/_unittest.py -v
