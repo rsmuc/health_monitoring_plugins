e3f1fa046ca4d1ca83069904402f58fd56c8cf96
HPmuc <Niederlande011@yahoo.de>
2016-07-15 09:10:04 +0200

    Update test_check_snmp_idrac.py

--- test/test_check_snmp_idrac.py
+++ test/test_check_snmp_idrac.py
@@ -8,11 +8,47 @@ from check_snmp_idrac import *
 import pytest
 import subprocess
 from testagent import *
-from types import MethodType
 
 # configuration of the testagent
 os.environ['MIBDIRS'] = os.path.dirname(os.path.abspath(__file__))
 configure(agent_address = "localhost:1234", rocommunity='public', rwcommunity='private')
+
+# create netsnmp Sessions for test_get and test_walk
+session = netsnmp.Session(Version=2, DestHost='localhost', Community='public')
+failSession = netsnmp.Session(Version=2, DestHost='1.2.3.4', Community='public')
+
+def get_system_uptime():
+    """
+    just a helper to get the system uptime in seconds
+    """
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = str(f.readline().split()[0])
+        uptime_seconds = uptime_seconds.replace('.', '')
+        return str(uptime_seconds)
+
+def test_get(capsys):
+    """
+    test of the get_data function
+    """
+    # run a get on a not existing host
+    with pytest.raises(SystemExit):
+        get_data(failSession, '.1', helper)
+    out, err = capsys.readouterr()    
+    assert 'Unknown - snmpget failed - no data for host' in out
+    # check if we receive the system uptime via snmp and compare it with the local uptime from /proc/uptime (except the last digit)
+    assert get_data(session, '.1.3.6.1.2.1.25.1.1.0', helper)[:-2] == get_system_uptime()[:-2]
+
+def test_walk(capsys):
+    """
+    test of the walk_data function
+    """
+    # run a walk on a not existing host
+    with pytest.raises(SystemExit):
+        assert walk_data(failSession, '.1', helper)
+    out, err = capsys.readouterr()
+    assert 'Unknown - snmpwalk failed - no data for host' in out
+    # check if we receive the system uptime via snmp and compare it with the local uptime from /proc/uptime (except the last digit)
+    assert walk_data(session, '.1.3.6.1.2.1.25.1.1', helper)[0][0][:-2] == get_system_uptime()[:-2]
 
 def test_start():
  # start the testagent
@@ -44,10 +80,15 @@ def test_start():
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.2 = STRING: "CPU 1"''')# location name of the temperature probe
     
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.3 = INTEGER: 3''')# status of the temperature probe
-    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "CPU 2"''')# location name of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.3 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "Inlet Temperature"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.1 = INTEGER: 3''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.1 = INTEGER: 228000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.1 = STRING: "PS 1 Voltage"''')# location name of the voltage probe
     
     start_server()
-    
+
 def test_system_test_idrac():
     # with -H 1.2.3.4 (unknown host)
     p=subprocess.Popen('health_monitoring_plugins/check_snmp_idrac/check_snmp_idrac.py -H 1.2.3.4', shell=True, stdout=subprocess.PIPE)
@@ -100,7 +141,12 @@ def test_everything_not_ok():
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.2 = STRING: "CPU 1"''')# location name of the temperature probe
     
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.3 = INTEGER: 5''')# status of the temperature probe
-    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "CPU 2"''')# location name of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.3 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "Inlet Temperature"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.1 = INTEGER: 5''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.1 = INTEGER: 228000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.1 = STRING: "PS 1 Voltage"''')# location name of the voltage probe
     
     p=subprocess.Popen('health_monitoring_plugins/check_snmp_idrac/check_snmp_idrac.py -H localhost:1234', shell=True, stdout=subprocess.PIPE)
     cmd_output = p.stdout.read()
@@ -108,8 +154,8 @@ def test_everything_not_ok():
     assert 'Critical - User assigned name: Main System Chassis - Typ: PowerEdge R420xr - Service tag: ABCD123. Global System status:\
  critical. System LCD status: critical. Global Storage status: critical. System Power status: off. Power unit "System Board PS\
  Redundancy" status: critical. Redundancy: lost. Chassis intrusion sensor "System Board Intrusion" is critical.\
- Cooling unit "System Board Fan Redundancy" status: critical. Temperature probe at "System Board Inlet Temp" is\
- criticalUpper.' in cmd_output
+ Cooling unit "System Board Fan Redundancy" status: critical. Temperature probe at "inlet temperature" is\
+ criticalUpper. Voltage probe at "ps 1 voltage" is criticalUpper' in cmd_output
     
 def test_overall_state_critical():
     """
@@ -144,7 +190,12 @@ def test_overall_state_critical():
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.2 = STRING: "CPU 1"''')# location name of the temperature probe
     
     register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.3 = INTEGER: 3''')# status of the temperature probe
-    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "CPU 2"''')# location name of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.3 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "Inlet Temperature"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.1 = INTEGER: 3''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.1 = INTEGER: 228000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.1 = STRING: "PS 1 Voltage"''')# location name of the voltage probe
     
     p=subprocess.Popen('health_monitoring_plugins/check_snmp_idrac/check_snmp_idrac.py -H localhost:1234', shell=True, stdout=subprocess.PIPE)
     cmd_output = p.stdout.read()
@@ -206,16 +257,102 @@ def test_cooling_unit_critical():
     
 def test_temperature_probe_critical():
     """
-    temperature probe 'CPU 1' status critical
+    temperature probe 'inlet temperature' status criticalUpper
     """
     
-    temperature_summary, temperature_long = temperature_probe_check([3,5,3],['Inlet Temp','CPU 1','CPU 2'])
+    unregister_all()
     
-    print 'summary: %s' % temperature_summary
-    print 'long: %s' % temperature_long
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.7.1 = STRING: "Main System Chassis"''')# user assigned name of the system chassis
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.9.1 = STRING: "PowerEdge R420xr"''')# system model type of the system chassis
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.11.1 = STRING: "ABCD123"''')# service tag name of the chassis
     
-    assert 'Temperature probe at "CPU 1" is critical' in temperature_summary
-    assert 'Temperature probe at "CPU 1" is critical' in temperature_long
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.1.0 = INTEGER: 3''')# overall rollup status of all components
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.2.0 = INTEGER: 3''')# system status as it is reflected by the LCD front panel
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.3.0 = INTEGER: 3''')# overall storage status
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.4.0 = INTEGER: 4''')# power state of the system
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.5.1 = INTEGER: 3''')# redundancy status of the power unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.7.1 = STRING: "System Board PS Redundancy"''')# name of the power unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.8.1 = INTEGER: 3''')# status of the power unit
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.70.1.5.1 = INTEGER: 3''')# status of the intrusion sensor
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.70.1.8.1 = STRING: "System Board Intrusion"''')# location of the intrusion sensor
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.10.1.7.1 = STRING: "System Board Fan Redundancy"''')# name of the cooling unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.10.1.8.1 = INTEGER: 3''')# status of the cooling unit
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.1 = INTEGER: 3''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.1 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.1 = STRING: "System Board Inlet Temp"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.2 = INTEGER: 3''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.2 = STRING: "CPU 1"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.3 = INTEGER: 5''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.3 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "Inlet Temperature"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.1 = INTEGER: 5''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.1 = INTEGER: 228000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.1 = STRING: "PS 1 Voltage"''')# location name of the voltage probe
+    
+    p=subprocess.Popen('health_monitoring_plugins/check_snmp_idrac/check_snmp_idrac.py -H localhost:1234', shell=True, stdout=subprocess.PIPE)
+    cmd_output = p.stdout.read()
+    print cmd_output
+    assert 'Temperature probe at "inlet temperature" is criticalUpper. ' in cmd_output
+    
+def test_voltage_probe_ciritcal():
+    """
+    voltage probe 'ps 2 voltage' status criticalUpper
+    """
+    
+    unregister_all()
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.7.1 = STRING: "Main System Chassis"''')# user assigned name of the system chassis
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.9.1 = STRING: "PowerEdge R420xr"''')# system model type of the system chassis
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.10.1.11.1 = STRING: "ABCD123"''')# service tag name of the chassis
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.1.0 = INTEGER: 3''')# overall rollup status of all components
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.2.0 = INTEGER: 3''')# system status as it is reflected by the LCD front panel
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.3.0 = INTEGER: 3''')# overall storage status
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.2.4.0 = INTEGER: 4''')# power state of the system
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.5.1 = INTEGER: 3''')# redundancy status of the power unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.7.1 = STRING: "System Board PS Redundancy"''')# name of the power unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.10.1.8.1 = INTEGER: 3''')# status of the power unit
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.70.1.5.1 = INTEGER: 3''')# status of the intrusion sensor
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.300.70.1.8.1 = STRING: "System Board Intrusion"''')# location of the intrusion sensor
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.10.1.7.1 = STRING: "System Board Fan Redundancy"''')# name of the cooling unit
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.10.1.8.1 = INTEGER: 3''')# status of the cooling unit
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.1 = INTEGER: 3''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.1 = STRING: "System Board Inlet Temp"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.2 = INTEGER: 3''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.2 = STRING: "CPU 1"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.5.1.3 = INTEGER: 3''')# status of the temperature probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.6.1.3 = INTEGER: 270''')
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.700.20.1.8.1.3 = STRING: "Inlet Temperature"''')# location name of the temperature probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.1 = INTEGER: 3''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.1 = INTEGER: 228000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.1 = STRING: "PS 1 Voltage"''')# location name of the voltage probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.2 = INTEGER: 5''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.6.1.2 = INTEGER: 241000''')# voltage probe reading
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.2 = STRING: "PS 2 Voltage"''')# location name of the voltage probe
+    
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.5.1.3 = INTEGER: 3''')# status of the voltage probe
+    register_snmpwalk_ouput('''iso.3.6.1.4.1.674.10892.5.4.600.20.1.8.1.3 = STRING: "PS 3 Voltage"''')# location name of the voltage probe
+    
+    p=subprocess.Popen('health_monitoring_plugins/check_snmp_idrac/check_snmp_idrac.py -H localhost:1234', shell=True, stdout=subprocess.PIPE)
+    cmd_output = p.stdout.read()
+    print cmd_output
+    assert 'Voltage probe at "ps 2 voltage" is criticalUpper' in cmd_output
+    assert 'Critical on ps 2 voltage'
     
 def test_stop():
     # stop the testagent
