058004a0eea442bbe1dcecbc0d43c5d418d0cfdf
Retakfual <Retakfual@users.noreply.github.com>
2016-08-02 13:32:11 +0200

    Update check_janitza.py

--- health_monitoring_plugins/check_janitza/check_janitza.py
+++ health_monitoring_plugins/check_janitza/check_janitza.py
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with check_meinberg_ntp.py.  If not, see <http://www.gnu.org/licenses/>.
+# along with check_janitza.py.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # Imports
