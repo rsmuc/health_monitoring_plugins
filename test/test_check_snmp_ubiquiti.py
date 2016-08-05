b97727328b0eacdf7b437aaf7572d0f258425cfb
rsmuc <rsmuc@mailbox.org>
2016-08-05 12:14:31 +0200

    fixed test ubuquiti

--- test/test_check_snmp_ubiquiti.py
+++ test/test_check_snmp_ubiquiti.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-# test_check_ubiquiti.py - test for Monitor a Ubiquity airMax.
+# test_check_snmp_ubiquiti.py - test for Monitor a Ubiquity airMax.
 
-# Copyright (C) 2016 Retakfual <https://github.com/Retakfual>
+# Copyright (C) 2016 Retakfual <https://github.com/Retakfual>, rsmuc
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -53,10 +53,9 @@ def test_start_ok_walk():
     walk_var +=  o + " = INTEGER: 4\n" 
   register_snmpwalk_ouput(walk_var)
   start_server()
+    
   
-  
-  
-  # tests the list if the listflag ist set
+# tests the list if the listflag is set
 def test_list_flag():
   p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -l", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   list_output =""
@@ -70,7 +69,7 @@ def test_list_flag():
 def test_ubiquiti_basics1():
   
   #without options
-  p = subprocess.Popen("health_monitoring_plugins/check_snmp_snmp_ubiquiti/check_snmp_ubiquiti.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   myout =  p.stdout.read()
   print "--------------------"
   print myout
@@ -79,18 +78,18 @@ def test_ubiquiti_basics1():
   assert "Unknown - Hostname must be specified" in myout
   
   # with host and no -t(does not depend if host is valid or not atm)
-  p = subprocess.Popen("health_monitoring_plugins/check_snmp_snmp_ubiquiti/check_snmp_ubiquiti.py -H 1.2.3.4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 1.2.3.4", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   assert "Unknown - Argument -t is missing or false!" in p.stdout.read()
   
-   # with  unknown host (-H 1.2.3.4) and -t qL1  
-  p = subprocess.Popen("health_monitoring_plugins/check_ubiquiti/check_ubiquiti.py -H 1.2.3.4 -t up", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  # with  unknown host (-H 1.2.3.4) and -t qL1  
+  p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 1.2.3.4 -t up", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   assert "Unknown - snmpget failed - no data for host 1.2.3.4 OID: .1.3.6.1.2.1.1.3.0" in p.stdout.read()
 
 
 # tests all the ubiquiti status
 def test_ubiquiti_ok_outputs():
   for n,u in zip(names,units):
-    p = subprocess.Popen("health_monitoring_plugins/check_snmp_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t "+n +" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t "+n +" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     assert "OK -  | \'type\'=4 "+u+" ;:10;:20;;" in p.stdout.read()
 
     
@@ -103,10 +102,11 @@ def test_start_critical_walk():
   
   register_snmpwalk_ouput(walk_var)
   
-  # tests all the 
+
+# tests all the 
 def test_critical_ubiquiti_outputs():
   for n,u in zip(names,units):
-    p = subprocess.Popen("health_monitoring_plugins/check_snmp_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t "+n +" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t "+n +" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     
     myoutput = p.stdout.read()
     print myoutput
@@ -124,9 +124,8 @@ def test_start_warning_walk():
   
   register_snmpwalk_ouput(walk_var)
   
-  
-  
-  # tests all the 
+
+# tests all the 
 def test_warning_ubiquiti_outputs():
   for n,u in zip(names,units):
     p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t "+n +" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -137,18 +136,19 @@ def test_warning_ubiquiti_outputs():
     
     assert "Warning - Warning on type | \'type\'=19 "+u+" ;:10;:20;;" in myoutput
     
-    
-    
-    
 def test_uptime():
   unregister_all()
   # starts the testagent and fills the oid with values
   walk_var="iso.3.6.1.2.1.1.3.0 = INTEGER: 4242424242\n" 
   register_snmpwalk_ouput(walk_var)
-  p = subprocess.Popen("health_monitoring_plugins/check_snmp_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t \"up\" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  p = subprocess.Popen("health_monitoring_plugins/check_snmp_ubiquiti/check_snmp_ubiquiti.py -H 127.0.0.1:1234 -t \"up\" --th metric=type,warning=:10,critical=:20", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   
   myoutput = p.stdout.read()
   print myoutput
   print "OK - Uptime = 49102 days, 3:10:42"
   
   assert "OK - Uptime = 49102 days, 3:10:42" in myoutput
+
+def test_stop_final():
+    # stop the testagent
+    stop_server()
