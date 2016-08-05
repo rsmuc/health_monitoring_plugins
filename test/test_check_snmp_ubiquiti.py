9bdb2b7691c9e024d2798d383a181c283ed9fe20
Retakfual <Retakfual@users.noreply.github.com>
2016-08-05 11:59:16 +0200

    Update test_check_snmp_ubiquiti.py

--- test/test_check_snmp_ubiquiti.py
+++ test/test_check_snmp_ubiquiti.py
@@ -21,7 +21,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('health_monitoring_plugins/check_snmp_ubiquiti'))
-from check_ubiquiti import *
+from check_snmp_ubiquiti import *
 
 import pytest
 import subprocess
